eae424541c9c887323dda7812b289c825f5391e1
Rebecca Lacer <r.a.lacer@hotmail.com>
2021-03-24 13:04:02 -0600

    adding weather  elements

--- script.js
+++ script.js
@@ -1,27 +1,68 @@
 window.addEventListener("load", function () {
-// anonymous function
+    // anonymous function
+    var existingHistory
+    if (!JSON.parse(localStorage.getItem("history"))) {
+        existingHistory = [];
 
-// parenthesis empty, not passing it a value
-function getSearchValue() {
-var searchValue = document.getElementById("form-input").value;
-if (searchValue) {
+    } else {
+        existingHistory = JSON.parse(localStorage.getItem('history'))
+    }
+    var historyItems = [];
 
-
-    // passing it searchValue
-searchWeather(searchValue);
-
-
-}
-
-
-}
-// anything being  passed to the function needs a parameter when function is written
-function searchWeather(searchValue) {
-// template literal or string literal - have backticks
-    var apiCall = `https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=32213800518e5adda67ca68fed9146b6`
-
-}
+    // parenthesis empty, not passing it a value
+    function getSearchValue() {
+        var searchValue = document.getElementById("form-input").value;
+        if (searchValue) {
 
 
-document.querySelector("#search-button").addEventListener("click", getSearchValue);
-}) 
+            // passing it searchValue
+            searchWeather(searchValue);
+
+
+        }
+
+
+    }
+    // anything being  passed to the function needs a parameter when function is written
+    function searchWeather(searchValue) {
+        // template literal or string literal - have backticks
+        var apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=imperial&appid=32213800518e5adda67ca68fed9146b6`
+        // adding fetch
+
+        fetch(apiCall)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                if (!existingHistory.includes(searchValue)) {
+                    // passing the function searchValue
+                    // handleHistory(searchValue)
+                }
+                todayEl = document.getElementById("today");
+                todayEl.textContent = "";
+
+                var titleEl = document.createElement("h3");
+                titleEl.classList.add("card-title");
+                todayEl.textContent = `${data.name} (${new Date().toLocaleDateString()})`;
+                console.log(todayEl);
+
+                var cardEl = document.createElement("div");
+                cardEl.classList.add("card");
+                var windEl = document.createElement("p");
+                windEl.classList.add("card-text");
+                var humidityEl = document.createElement('p');
+                humidityEl.classList.add("card-text");
+                var temperatureEl = document.createElement("p");
+                temperatureEl.classList.add("card-text");
+                
+
+
+
+
+
+            })
+    }
+
+    document.querySelector("#search-button").addEventListener("click", getSearchValue);
+})
